f73a433f5770cc5f54dfa7eebd89809bb6c23eeb
Hariel007 <Hariel06@outlook.com>
2023-01-25 09:53:58 -0300

    Parte de Front-End iniciada

---
+++ resources/views/products.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('title','Produtos')
+
+@section('content')
+
+
+ @if($busca != '')
+     <P>O usuário está buscando por: {{ $busca }}</P>
+ @endif
+
+
+@endsection
+
